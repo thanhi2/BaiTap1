4d29c5b5cb42b5d1399df2620461ed2ac16e537d
nhi34466@gmail.com <nhi34466@gmail.com>
2023-05-23 10:14:27 +0700

    BaiTap1

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product, products } from '../products';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent {
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
+}
+export class ProductListComponent {
+
+  products = [...products];
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+}
