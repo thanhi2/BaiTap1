acbbe1652ac9aa949950bf4b17dfec38493d9806
nhi34466@gmail.com <nhi34466@gmail.com>
2023-05-26 11:19:06 +0700

    Hoan thanh bai 1

---
+++ hello/README.md
@@ -0,0 +1,3 @@
+# BT
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/3wgtew)
---
+++ hello/src/app/shipping/shipping.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-shipping',
+  templateUrl: './shipping.component.html',
+  styleUrls: ['./shipping.component.scss']
+})
+export class ShippingComponent {
+
+  shippingCosts!: Observable<{ type: string, price: number }[]>;
+
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.shippingCosts = this.cartService.getShippingPrices()
+  }
+}
